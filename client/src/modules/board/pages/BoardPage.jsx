d165c9fdc8ec234e72615056e717a2d3b013057c
Dina Hussam <81252117+Dinahussam@users.noreply.github.com>
2024-04-20 19:19:34 +0200

    style

--- client/src/modules/board/pages/BoardPage.jsx
+++ client/src/modules/board/pages/BoardPage.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import BoardTable from '../components/BoardTable'
+import './BoardPage.css';
 
 const BoardPage = () => {
   return (
+  <div className='board-page'>
     <BoardTable/>
+    </div>
   )
 }
 
