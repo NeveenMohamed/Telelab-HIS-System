283d0ee161854b70ac00ff7674df1f2edd6adc80
Dina Hussam <81252117+Dinahussam@users.noreply.github.com>
2024-04-22 00:06:25 +0200

    table style with icons

--- client/src/modules/board/components/BoardTable.jsx
+++ client/src/modules/board/components/BoardTable.jsx
@@ -9,6 +9,9 @@ import Paper from '@mui/material/Paper';
 import { CiAirportSign1 } from "react-icons/ci";
 import { useEffect, useState } from 'react';
 import axios from '../../../core/api/api';
+import './BoardTable.css'
+import { GrInProgress } from "react-icons/gr";
+import { AiOutlineFileDone } from "react-icons/ai";
 
 
 
@@ -21,7 +24,7 @@ const BoardTable = () => {
         createData('Frozen yoghurt', 1, 6.0, 24, 4.0),
         createData('Ice cream sandwich', 2, 9.0, 37, 4.3),
         createData('Eclair', 1, 16.0, 24, 6.0),
-        createData('Cupcake', 3, 3.7, 67, 4.3),
+        createData('Cupcake', 2, 3.7, 67, 4.3),
         createData('Gingerbread', 2, 16.0, 49, 3.9),]
 
     useEffect(() => {
@@ -36,39 +39,41 @@ const BoardTable = () => {
     const [labs, setLabs] = useState()
         
     return (
+    <div className='table-container'>
         <TableContainer component={Paper}>
-            <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                <TableHead>
+            <Table className='table' sx={{ minWidth: 650 }} aria-label="simple table">
+                <TableHead className='TableHeadCss'>
                     <TableRow>
-                        <TableCell>Dessert (100g serving)</TableCell>
-                        <TableCell align="right">status</TableCell>
-                        <TableCell align="right">Fat</TableCell>
-                        <TableCell align="right">Carbs</TableCell>
-                        <TableCell align="right">Protein</TableCell>
-                        <TableCell align="right">Result</TableCell>
+                        <TableCell sx={{ color: 'white' , fontWeight: 'bold' , fontSize: 16 , textAlign:"left"}}>Dessert (100g serving)</TableCell>
+                        <TableCell sx={{ color: 'white' , fontWeight: 'bold' , fontSize: 16 , textAlign:"center"}} align="right">status</TableCell>
+                        <TableCell sx={{ color: 'white' , fontWeight: 'bold' , fontSize: 16 , textAlign:"center"}} align="right">Fat</TableCell>
+                        <TableCell sx={{ color: 'white' , fontWeight: 'bold' , fontSize: 16 , textAlign:"center"}} align="right">Carbs</TableCell>
+                        <TableCell sx={{ color: 'white' , fontWeight: 'bold' , fontSize: 16 , textAlign:"center"}} align="right">Protein</TableCell>
+                        <TableCell sx={{ color: 'white' , fontWeight: 'bold' , fontSize: 16 , textAlign:"center"}} align="right">Result</TableCell>
                     </TableRow>
                 </TableHead>
                 <TableBody>
                     {patients.map((patient) => (
-                        <TableRow
+                        <TableRow className='TableRowCss'
                             key={patient.name}
                             sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
                         >
                             <TableCell component="th" scope="row">
                                 {patient.name}
                             </TableCell>
-                            <TableCell align="right">{patient.status == 1 ? <CiAirportSign1 size={20} /> : patient.status == 2 ? <CiAirportSign1 /> : <CiAirportSign1 />} </TableCell>
-                            <TableCell align="right">{patient.fat}</TableCell>
-                            <TableCell align="right">{patient.carbs}</TableCell>
-                            <TableCell align="right">{patient.protein}</TableCell>
-                            <TableCell align="right">
-                                <button>View Result</button>
+                            <TableCell align="center">{patient.status == 2 ? <AiOutlineFileDone className='DoneIcon' size={28}/> :  <GrInProgress className='GrInProgressIcon' size={28}/>} </TableCell>
+                            <TableCell align="center">{patient.fat}</TableCell>
+                            <TableCell align="center">{patient.carbs}</TableCell>
+                            <TableCell align="center">{patient.protein}</TableCell>
+                            <TableCell align="center">
+                                <button className='buttonCss'>View Result</button>
                             </TableCell>
                         </TableRow>
                     ))}
                 </TableBody>
             </Table>
         </TableContainer>
+        </div>
     )
 }
 
