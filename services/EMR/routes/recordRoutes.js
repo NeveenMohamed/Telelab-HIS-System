6a8045a9b0d23cfa2035a47ffb23881331a60a9e
sherif <sherifahmedelsayed2@gmail.com>
2024-05-01 18:04:00 +0300

    Update recordRoutes.js

--- services/EMR/routes/recordRoutes.js
+++ services/EMR/routes/recordRoutes.js
@@ -2,13 +2,15 @@ const express = require("express");
 const router = express.Router();
 const RecordController = require("../controllers/recordController");
 
-router.post("/", RecordController.createRecord);
-
-router.get("/lab/:labId", RecordController.getRecordsByLabID);
+router.get("/", RecordController.getAllRecords);
 
 router.get(
   "/appointment/:appointmentId",
   RecordController.getRecordsByAppointmentID
 );
+
+router.post("/", RecordController.createRecord);
+
+router.get("/lab/:labId", RecordController.getRecordsByLabID);
 
 module.exports = router;
