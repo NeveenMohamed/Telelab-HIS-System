93c6ad77ed6fda089f9582ca8796c66c5affa2ae
sherif <sherifahmedelsayed2@gmail.com>
2024-05-01 18:00:15 +0300

    Update appointmentRoutes.js

--- services/Appointments/routes/appointmentRoutes.js
+++ services/Appointments/routes/appointmentRoutes.js
@@ -2,10 +2,16 @@ const express = require("express");
 const router = express.Router();
 const AppointmentController = require("../controllers/appointmentController");
 
+router.post("/", AppointmentController.bookAppointment);
+
+router.get("/", AppointmentController.getAllAppointments);
+
 router.get(
   "/appointment/:appointmentId",
   AppointmentController.getAppointmentByID
 );
+
+router.delete("/:appointmentId", AppointmentController.cancelAppointment);
 
 router.get("/lab/:labId", AppointmentController.getAppointmentByLabID);
 
