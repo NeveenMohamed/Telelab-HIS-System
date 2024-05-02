34905dc4bff309256588d55d321e8dc4f58e386c
NeveenMohamed <neveen.hassan02@eng-st.cu.edu.eg>
2024-05-02 02:59:33 +0200

    update index.js in back

--- services/client.js
+++ services/client.js
@@ -1,66 +1,63 @@
-const MongoClient = require("mongodb").MongoClient;
+// const mongoose = require("mongoose");
+// var net = require("net");
 
-var net = require("net");
+// // Create a TCP client
+// const client_TCP = new net.Socket();
 
-// Create a TCP client
-const client = new net.Socket();
+// // Connect to the server
+// const PORT = 3838;
+// const HOST = "41.46.96.67";
 
-// Connect to the server
-const PORT = 3838;
-const HOST = "41.46.96.67";
+// client_TCP.connect(PORT, HOST, () => {
+//   console.log("Connected to server from client file");
+//   // No need to send initial message here
+// });
 
-client.connect(PORT, HOST, () => {
-  console.log("Connected to server from client file");
-  // No need to send initial message here
-});
+// // Handle data from the server
+// client_TCP.on("data", (data) => {
+//   console.log("Received from server:", data.toString());
+// });
 
-// Handle data from the server
-client.on("data", (data) => {
-  console.log("Received from server:", data.toString());
-});
+// // Handle connection closure
+// client_TCP.on("close", () => {
+//   console.log("Connection closed");
+// });
 
-// Handle connection closure
-client.on("close", () => {
-  console.log("Connection closed");
-});
+// // Function to watch all collections for changes
+// async function watchAllCollections() {
+//   try {
+//       const db = mongoose.connection;
 
-// Function to watch all collections for changes
-async function watchAllCollections(url) {
-  // Use connect method to connect to the server
-  const client = new MongoClient(url, { useUnifiedTopology: true });
+//       db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
-  try {
-    // Connect to the MongoDB server
-    await client.connect();
+//       // Get a list of all collections in the database
+//       const collections = await mongoose.connection.db.listCollections().toArray();
 
-    console.log("Connected to MongoDB server from client file");
+//       // Watch each collection for changes
+//       collections.forEach(async (collectionInfo) => {
+//           const collection = db.collection(collectionInfo.name);
+//           const changeStream = collection.watch();
 
-    // Get the database
-    const db = client.db();
+//           changeStream.on("change", (change) => {
+//               console.log(`Change detected in ${collectionInfo.name} collection:, change`);
 
-    // Get a list of all collections in the database
-    const collections = await db.listCollections().toArray();
+//               // Send the change data to all connected clients
+//               for (const socket of sockets) {
+//                   socket.write(JSON.stringify(change));
+//               }
 
-    // Watch each collection for changes
-    collections.forEach((collectionInfo) => {
-      const changeStream = db.collection(collectionInfo.name).watch();
-
-      changeStream.on("change", (change) => {
-        console.log(
-          `Change detected in ${collectionInfo.name} collection:`,
-          change
-        );
-        client.write(JSON.stringify(change));
-      });
-    });
-  } catch (error) {
-    console.error("Error reading data:", error);
-  }
-}
-
-module.exports={watchAllCollections};
-
-// finally {
-//   // Close the client
-//   await client.close();
+//               // socket.write(JSON.stringify(change));
+//           });
+//       });
+//   } catch (error) {
+//       console.error("Error connecting to MongoDB:", error);
+//   }
 // }
+
+
+// module.exports={watchAllCollections};
+
+// // finally {
+// //   // Close the client
+// //   await client.close();
+// // }
--- services/index.js
+++ services/index.js
@@ -1,14 +1,15 @@
-// Imports
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+var net = require("net");
+
 
 const app = express();
 
 const appointmentRoute = require("./Appointments/routes/appointmentRoutes");
 const userRoute = require("./Registeration/routes/userRoutes");
 const recordRoute = require("./EMR/routes/recordRoutes");
-const { watchAllCollections} = require("./client");
+
 
 // Connect to database
 const url =
@@ -20,7 +21,7 @@ mongoose
   .then(() => {
     console.log("Connected to MongoDB from index file");
     app.listen(port, () => console.log("Back Server up and running"));
-    watchAllCollections(url);
+    watchAllCollections();
     console.log("Listening....");
   })
   .catch((error) => {
@@ -35,3 +36,54 @@ app.use(cors());
 app.use("/appointments", appointmentRoute);
 app.use("/user", userRoute);
 app.use("/record", recordRoute);
+
+
+
+// Create a TCP client
+const client_TCP = new net.Socket();
+
+// Connect to the server
+const PORT = 3838;
+const HOST = "41.46.96.67";
+
+client_TCP.connect(PORT, HOST, () => {
+  console.log("Connected to server from client file");
+  // No need to send initial message here
+});
+
+// Handle data from the server
+client_TCP.on("data", (data) => {
+  console.log("Received from server:", data.toString());
+});
+
+// Handle connection closure
+client_TCP.on("close", () => {
+  console.log("Connection closed");
+});
+
+// Function to watch all collections for changes
+async function watchAllCollections() {
+  try {
+      const db = mongoose.connection;
+
+      db.on("error", console.error.bind(console, "MongoDB connection error:"));
+
+      // Get a list of all collections in the database
+      const collections = await mongoose.connection.db.listCollections().toArray();
+
+      // Watch each collection for changes
+      collections.forEach(async (collectionInfo) => {
+          const collection = db.collection(collectionInfo.name);
+          const changeStream = collection.watch();
+
+          changeStream.on("change", (change) => {
+              console.log(`Change detected in ${collectionInfo.name} collection:, change`);
+              client_TCP.write(JSON.stringify(change.fullDocument));
+          });
+      });
+  } catch (error) {
+      console.error("Error connecting to MongoDB:", error);
+  }
+}
+
+
